5a9d8bda59f2998ce53540d519a75f7c941ff7f7
Demo <51279632+Demo4889@users.noreply.github.com>
2019-08-08 22:42:53 -0400

    html files
    Updated

--- html/js/app.js
+++ html/js/app.js
@@ -1,7 +1,7 @@
 $(".character-box").hover(
     function() {
         $(this).css({
-            "background": "rgba(42, 125, 193, 1.0)",
+            "background": "rgba(42, 125, 193, 0.6)",
             "transition": "200ms",
         });
     }, function() {
@@ -47,7 +47,7 @@ $("#deletechar").click(function () {
             $.each(data.characters, function (index, char) {
                 if (char.charid !== 0) {
                     var charid = char.identifier.charAt(4);
-                    $('[data-charid=' + charid + ']').html('<h3 class="character-fullname">'+ char.firstname +' '+ char.lastname +'</h3><div class="character-info"><p class="character-info-work"><strong>Work: </strong><span>'+ char.job + ' - ' +  char.job_grade +'</span></p><p class="character-info-money"><strong>Cash: </strong><span>'+ char.money +'</span></p><p class="character-info-bank"><strong>Bank: </strong><span>'+ char.bank +'</span></p> <p class="character-info-dateofbirth"><strong>Date of birth: </strong><span>'+ char.dateofbirth +'</span></p> <p class="character-info-gender"><strong>Gender: </strong><span>'+ char.sex +'</span></p></div>').attr("data-ischar", "true");
+                    $('[data-charid=' + charid + ']').html('<h3 class="character-fullname">'+ char.firstname +' '+ char.lastname +'</h3><div class="character-info"><p class="character-info-work"><strong>Job: </strong><span>'+ char.job +'</span><br><strong>Grade: </strong><span> '+ char.job_grade +'</span></p><p class="character-info-money"><strong>Cash: </strong><span>'+ char.money +'</span></p><p class="character-info-bank"><strong>Bank: </strong><span>'+ char.bank +'</span></p> <p class="character-info-dateofbirth"><strong>DoB: </strong><span>'+ char.dateofbirth +'</span></p> <p class="character-info-gender"><strong>Gender: </strong><span>'+ char.sex +'</span></p></div>').attr("data-ischar", "true");
                 }
             });
         }
